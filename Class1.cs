ae10cfc951be85df2c2afc978777754956ad3a95
Nikita-hubn <73433620+Nikita-hubn@users.noreply.github.com>
2023-04-29 13:51:38 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Class1
+    {
+        private string _surname;
+        private int _curs;
+        private int _bal;
+        public Class1() //конструктор по умолчанию
+        {
+
+        }
+
+        public Class1(string surname, int curs, int bal) //конструктор с параметрами
+        {
+            _surname = surname;
+            _curs = curs;
+            _bal = bal;
+        }
+        public string GetSurname()
+        {
+            return _surname;
+        }
+
+        public void SetSurname(string surname)
+        {
+            _surname = surname;
+        }
+        public int GetCurs()
+        {
+            return _curs;
+        }
+
+        public void SetCurs(int curs)
+        {
+            _curs = curs;
+        }
+
+        public int GetBal()
+        {
+            return _bal;
+        }
+
+        public void SetBal(int bal)
+        {
+            _bal = bal;
+        }
+        public virtual double QualityOfStudent() //функция, которая определяет качество объекта по заданной формуле для класса 1-го уровня
+        {
+            double quality = 0.2 * GetCurs() * GetBal();
+            return quality;
+        }
+        public virtual string MakeStr() // возвращает строку для вывода в листбокс класса 1-го уровня
+        {
+            return "Фамилия: " + GetSurname() + ", Курс: " + GetCurs() + ", Средний бал: " + GetBal();
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Class2 : Class1
+    {
+        private Boolean _English=false;
+        public Class2(string firm, int diagonal, int soundPower, int English) : base(firm, diagonal, soundPower) //конструктор
+        {
+            if(English>0)
+            {
+                _English = true;
+            }
+        }
+        public Boolean GetEn()
+        {
+            return _English;
+        }
+        public override double QualityOfStudent()
+        {
+            double quality = base.QualityOfStudent();
+            double qualityEd;
+            if (_English == true)
+            {
+                qualityEd = 2 * quality;
+            }
+            else
+            {
+                qualityEd = 0.9 * quality;
+            }
+            return qualityEd;
+        }
+        public override string MakeStr() // возвращает строку для вывода в листбокс класса 2-го уровня
+        {
+            return base.MakeStr() + ", Английский: " + GetEn();
+        }
+    }
+}
